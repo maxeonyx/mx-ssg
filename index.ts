87be27141a63e0acdb298bd15f1fd130877ec760
Maxwell Clarke <maxeonyx@gmail.com>
2022-02-15 06:54:42 +1300

    my goodness it works

--- index.ts
+++ index.ts
@@ -1,47 +1,48 @@
-import { HTMLElement, Node, parse } from 'node-html-parser';
+import { DomUtils, parseDocument } from 'htmlparser2';
+import { Document, isTag, Element, Node } from 'domhandler';
+import render from 'dom-serializer';
 import fs from 'fs';
 
-function parse_file(name: string) {
+function parse_file(name: string): Document {
     let text = fs.readFileSync('templates/src/' + name, 'utf8');
 
-    let dom = parse(text);
+    let dom = parseDocument(text);
 
     return dom;
 }
 
-function get_template(src: string): HTMLElement | undefined {
+function get_template(src: string): Document | undefined {
     return parse_file(src);
 }
 
 function do_templating(
     filename: string,
-    doc: HTMLElement,
+    doc: Document,
 ) {
     let i = 0;
     while (true) {
         i += 1;
         console.log('=================================================');
         console.log(`<!-- ${i} A -->`);
-        // console.log(doc);
-        console.log(doc.toString());
+        console.log(render(doc));
 
-        let use_el = doc.getElementsByTagName('mx-use')[0] as HTMLElement;
+        let use_els = DomUtils.getElementsByTagName("mx-use", doc)
 
-        if (use_el === null) {
+        if (use_els.length === 0) {
             console.log(`DONE`);
             return;
         }
+        let use_el = use_els[0];
         let parent = use_el.parentNode;
-        console.log('USE EL');
+        // console.log('USE EL');
         // console.log(use_el);
-        console.log(use_el.toString());
 
-        let fills_arr = use_el.querySelectorAll('> mx-fill');
-        let fills: { [k: string]: HTMLElement } = {};
+        let fills_arr = DomUtils.getChildren(use_el).filter(c => isTag(c) && c.tagName == "mx-fill") as Element[]
+        let fills: { [k: string]: Element } = {};
         for (let fill of fills_arr) {
             // remove the fills, they shouldn't be in the node anymore so that we can use everything else for the default slot
-            fill.remove();
-            let name = fill.getAttribute('name');
+            DomUtils.removeElement(fill);
+            let name = DomUtils.getAttributeValue(fill, "name");
             if (name === undefined) {
                 throw new Error(
                     `<mx-fill> has no 'name' attribute in ${filename}`,
@@ -55,7 +56,7 @@ function do_templating(
         console.log(`${fills_arr.length + 1} fills`);
         console.log();
 
-        let src = use_el.getAttribute('src');
+        let src = DomUtils.getAttributeValue(use_el, "src");
         if (src === undefined) {
             throw new Error(`<mx-use> has no 'src' attribute in ${filename}`);
         }
@@ -67,48 +68,42 @@ function do_templating(
         }
 
         console.log(`<!-- ${i} C: Load template "${src}" -->`);
-        // console.log(template);
-        console.log(template.toString());
+        console.log(render(template));
+        // console.log(template.toString());
 
-        const idx = parent.childNodes.findIndex((child: Node) => {
-            return child === use_el;
-        });
-        parent.childNodes = [
-            ...parent.childNodes.slice(0, idx),
-            ...template.childNodes,
-            ...parent.childNodes.slice(idx + 1),
-        ];
+        DomUtils.replaceElement(use_el, template);
 
         console.log(`<!-- ${i} D: Replace use with template -->`);
-        // console.log(doc);
-        console.log(doc.toString());
+        console.log(render(doc));
+        // console.log(doc.toString());
 
-        let slots = template.querySelectorAll('mx-slot');
+        let slots = DomUtils.getElementsByTagName('mx-slot', doc);
 
-        slots.forEach((slot_el: HTMLElement) => {
-            let name = slot_el.getAttribute('name');
+        slots.forEach((slot_el: Element) => {
+            let name = DomUtils.getAttributeValue(slot_el, "name");
 
-            let nodes = undefined;
+            let nodes: Node[] | undefined = undefined;
             if (name === undefined) {
                 if (use_el) { // will always succeed - deno has type inference error without this
-                    nodes = use_el.childNodes;
+                    nodes = use_el.children;
                 }
             } else {
                 if (fills.hasOwnProperty(name)) {
-                    nodes = fills[name].childNodes;
+                    nodes = fills[name].children;
                 }
             }
 
-            if (nodes === undefined) {
-                slot_el.remove();
-            } else {
-                slot_el.replaceWith(...nodes);
+            if (nodes !== undefined) {
+                for (let node of nodes) {
+                    DomUtils.append(slot_el, node);
+                }
             }
+            DomUtils.removeElement(slot_el);
         });
 
         console.log(`<!-- ${i} E: Replace slots with fills -->`);
-        // console.log(doc);
-        console.log(doc.toString());
+        console.log(render(doc));
+        // console.log(doc.toString());
     }
 }
 
